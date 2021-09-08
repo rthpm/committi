187a023a85eebae8d9ff1a222945ace3b333f312
rthpm <rthp@fastmail.com>
2021-09-08 12:34:50 +0200

    Styling cleanup: alignments

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:sessions, :registrations]
   as :user do
     # Sensible reroute
-    get 'login', to: 'devise/sessions#new', as: :new_user_session
-    post 'login', to: 'devise/sessions#create', as: :user_session
-    get 'signup', to: 'devise/registrations#new', as: :new_user_registration
-    post 'signup' => 'devise/registrations#create', :as => :user_registration
-    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get    'login'  => 'devise/sessions#new',         :as => :new_user_session
+    post   'login'  => 'devise/sessions#create',      :as => :user_session
+    get    'signup' => 'devise/registrations#new',    :as => :new_user_registration
+    post   'signup' => 'devise/registrations#create', :as => :user_registration
+    delete 'logout' => 'devise/sessions#destroy',     :as => :destroy_user_session
 
     # Authenticated users gets a pass to a dedicated /profile
     authenticated :user do
