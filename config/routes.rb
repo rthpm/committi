0d2cbd2030f0ef1e84b75fd0ff6949c1a96c7a69
rthpm <rthp@fastmail.com>
2021-09-08 12:28:08 +0200

    Beautiful and sensible routing for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions, :registrations]
+  as :user do
+    # Sensible reroute
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    get 'signup', to: 'devise/registrations#new', as: :new_user_registration
+    post 'signup' => 'devise/registrations#create', :as => :user_registration
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    # Authenticated users gets a pass to a dedicated /profile
+    authenticated :user do
+      get 'profile' => 'users/profiles#index', :as => :user_profile
+    end
+  end
 end
