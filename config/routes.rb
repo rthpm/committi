dd7ff7390e2aab461761f41451f7240280132092
hanxcity <hanxcity@gmail.com>
2021-09-08 20:18:32 +0200

    change of routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :user, :path => '', :path_names => { :sign_in => "login",
-                                                  :sign_out => "logout",
-                                                  :sign_up => "signup", }
-  get '/:username' => 'users#show', :as => 'profile'
+  root: 'homes#index'
+  devise_for :users, skip: [:sessions, :registrations]
+  as :user do
+  
+    # Sensible reroute
+    get    'login'  => 'devise/sessions#new',         :as => :new_user_session
+    post   'login'  => 'devise/sessions#create',      :as => :user_session
+    get    'signup' => 'devise/registrations#new',    :as => :new_user_registration
+    post   'signup' => 'devise/registrations#create', :as => :user_registration
+    delete 'logout' => 'devise/sessions#destroy',     :as => :destroy_user_session
+
+    # Authenticated users gets a pass to a dedicated /profile
+    authenticated :user do
+      get 'profile' => 'users/profiles#index', :as => :user_profile
+    end
+  end
 end
