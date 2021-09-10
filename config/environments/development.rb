508ed8aa51808e31ac51a095bbfb8384c581241f
NASSIM SNACEL <70256670+Nass13014@users.noreply.github.com>
2021-09-10 15:46:26 +0200

    update development.rb letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -32,6 +35,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  # action_mailer_letter_opener => 
+  config.action_mailer.delivery_method = :letter_opener 
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
