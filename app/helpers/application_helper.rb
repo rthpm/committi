3600cb55afbea5a5a1d70ee33f19df8aee61b3a0
rthpm <rthp@fastmail.com>
2021-09-14 20:37:44 +0200

    Added a naive daily rate limiting for status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def enrolled?(challenge, user)
     Enrollment.where(challenge: challenge, user: user).exists?
   end
+
+  def can_post?(challenge, user)
+    status = Status.where(user: user, 
+                          challenge: challenge).order(created_at: :desc).first.created_at.to_date
+    status != Time.now.to_date
+  end
 end
