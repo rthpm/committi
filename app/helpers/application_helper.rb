7e85471b52b940aa6a953a62b0ebb3aa0caa63ef
rthpm <rthp@fastmail.com>
2021-09-15 09:21:08 +0200

    Fixed the can_post? method to allow a first status to be posted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,8 @@ module ApplicationHelper
   end
 
   def can_post?(challenge, user)
-    status = Status.where(user: user, 
-                          challenge: challenge).order(created_at: :desc).first.created_at.to_date
-    status != Time.now.to_date
+    status = Status.where(user: user, challenge: challenge)
+    return true unless status.exists?
+    status.order(created_at: :desc).first.created_at.to_date != Time.now.to_date
   end
 end
